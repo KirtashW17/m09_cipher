2913b3c346672f98c41d1d0ce12183cfac7968ea
Thomas Scalise <tscalise@ingent.net>
2021-11-14 22:17:47 +0100

    Cipher and Hash controller done

--- src/main/java/tscalise/cipherProject/controllers/HashingController.java
+++ src/main/java/tscalise/cipherProject/controllers/HashingController.java
@@ -1,13 +1,17 @@
 package tscalise.cipherProject.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import tscalise.cipherProject.libraries.hashing.HashAlgorithm;
+import tscalise.cipherProject.libraries.hashing.ShaHashing;
+
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class HashingController {
-
     @FXML
     private RadioButton RBsha1;
     @FXML
@@ -16,12 +20,83 @@ public class HashingController {
     private RadioButton RBsha256;
     @FXML
     private RadioButton RBsha512;
+    @FXML
+    private CheckBox CBseed;
+    @FXML
+    private RadioButton RBrandom;
+    @FXML
+    private ToggleGroup TGseedMode;
+    @FXML
+    private RadioButton RBmanual;
+    @FXML
+    private TextField TFseed;
+    @FXML
+    private TextArea TAinput;
+    @FXML
+    private TextArea TAoutput;
+
+    // TODO: COPY BUTTON
 
 
     @FXML
     public void exit() {
         Stage stage = (Stage) RBsha1.getScene().getWindow() ;
         stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
+    }
+
+    @FXML
+    public void pressActionButton() throws NoSuchAlgorithmException {
+        String input, output, algorithm;
+        byte[] seed = null;
+
+        input = TAinput.getText();
+
+        RadioButton selectedRB = (RadioButton) TGhashAlgorythm.getSelectedToggle();
+        algorithm = selectedRB.getText();
+
+        if (CBseed.isSelected()) {
+            if (RBmanual.isSelected()) {
+                seed = TFseed.getText().getBytes(StandardCharsets.UTF_8);
+            } else {
+                byte[] randomBytes = new byte[16];
+                Random random = new Random();
+                random.nextBytes(randomBytes);
+                seed = randomBytes;
+            }
+        }
+
+        output = ShaHashing.calculateHashHex(input.getBytes(StandardCharsets.UTF_8), seed, HashAlgorithm.valueOf(algorithm));
+        TAoutput.setText(output);
+    }
+
+    @FXML
+    public void seedCBAction() {
+        if (CBseed.isSelected()) {
+            RBrandom.setDisable(false);
+            RBmanual.setDisable(false);
+            if (RBmanual.isSelected())
+                TFseed.setDisable(false);
+        } else {
+            RBrandom.setDisable(true);
+            RBmanual.setDisable(true);
+            TFseed.setDisable(true);
+        }
+    }
+
+    @FXML
+    public void swtchSeedModeRB() {
+        TFseed.setDisable(!RBmanual.isSelected());
+    }
+
+    @FXML
+    public void pressClearButton() {
+        TFseed.clear();
+        TAoutput.clear();
+        TAoutput.clear();
+        RBsha1.setSelected(true);
+        RBmanual.setSelected(false);
+        CBseed.setSelected(false);
+        seedCBAction();
     }
 
 }
--- src/main/java/tscalise/cipherProject/controllers/KeyedVigenereController.java
+++ src/main/java/tscalise/cipherProject/controllers/KeyedVigenereController.java
@@ -4,21 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import tscalise.cipherProject.libraries.cipher.KeyedVigenere;
 
 public class KeyedVigenereController {
 
     @FXML
     private RadioButton RBcrypt;
     @FXML
-    private ToggleGroup TGcipherAction;
+    private TextField TFalphabetModificator;
     @FXML
-    private RadioButton RBdecrypt;
-    @FXML
-    private TextField TFalphabetKey;
-    @FXML
-    private Button BtnCrypt;
-    @FXML
-    private Button BtnExit;
+    private Button btnAction;
     @FXML
     private TextField TFpassphrase;
     @FXML
@@ -28,11 +23,57 @@ public class KeyedVigenereController {
     @FXML
     private TextArea TAoutput;
 
+    // TODO: COPY BUTTON
+
+
 
     @FXML
-    public void exit() {
+    public void pressExitButton() {
         Stage stage = (Stage) TAinput.getScene().getWindow() ;
         stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
+    }
+
+    @FXML
+    public void pressActionButton() {
+        String alphabetModificator, passphrase, input, output;
+        
+        alphabetModificator = TFalphabetModificator.getText();
+        passphrase = TFpassphrase.getText();
+        input = TAinput.getText();
+
+        KeyedVigenere keyedVigenere = new KeyedVigenere(alphabetModificator, passphrase);
+
+        if (RBcrypt.isSelected())
+            output = keyedVigenere.cryptMessage(input);
+        else
+            output = keyedVigenere.decryptMessage(input);
+
+        TAoutput.setText(output);
+    }
+
+    @FXML
+    public void pressClearButton() {
+        TAinput.clear();
+        TAoutput.clear();
+        TFalphabetModificator.clear();
+        TFpassphrase.clear();
+        RBcrypt.setSelected(true);
+        typeOnAlphabetModificator();
+        switchRadioButtons();
+    }
+
+    @FXML
+    public void typeOnAlphabetModificator() {
+        String alphabetUsed = new String(KeyedVigenere.generateAlphabet(TFalphabetModificator.getText()));
+        LabelAlphabetUsed.setText(alphabetUsed);
+    }
+
+    @FXML
+    public void switchRadioButtons() {
+        if (RBcrypt.isSelected())
+            btnAction.setText("Cifrar");
+        else
+            btnAction.setText("Descifrar");
     }
 
 
